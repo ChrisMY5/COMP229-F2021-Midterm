1ba63a2ee4e8af6de1d3211217561f8c7aab6551
Chris MY Oh <carlsearin@hotmail.com>
2021-10-27 00:27:27 -0400

    Connect to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  //Point MongoDB Atlas. Connect to database on MongoDB 
+  "URI": "mongodb+srv://Admin:5fa8a98BQ75bjaTE@cluster0.1lgvj.mongodb.net/book_store?retryWrites=true&w=majority"
 };
